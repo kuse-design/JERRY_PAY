49e7fc3e0187b857ecfd1f5ae752bf46a26baa09
Kuse_design <0luwakusejerry1@gmail.com>
2026-05-01 19:03:03 +0100

    "task"

--- src/main/java/com/example/jerry_pay/data/models/User.java
+++ src/main/java/com/example/jerry_pay/data/models/User.java
@@ -1,4 +1,20 @@
 package com.example.jerry_pay.data.models;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Document
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private int age;
+    private LocalDateTime createdAt;
+
+
+
 }
