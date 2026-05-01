6f31b1efd3a934599f91ddd0c6c800ee45a134fc
Kuse_design <0luwakusejerry1@gmail.com>
2026-05-01 19:02:51 +0100

    "task"

--- src/main/java/com/example/jerry_pay/data/models/Transaction.java
+++ src/main/java/com/example/jerry_pay/data/models/Transaction.java
@@ -1,4 +1,18 @@
 package com.example.jerry_pay.data.models;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Document
 public class Transaction {
+    private Long id;
+    private TransactionType type;
+    private BigDecimal amount;
+    private String reference;
+    private LocalDateTime timestamp;
+
 }
