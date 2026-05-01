73ee1038cb3e835fc1fb8fc21ec0452c3608f0d4
Kuse_design <0luwakusejerry1@gmail.com>
2026-05-01 20:04:55 +0100

    "task"

--- src/main/java/com/example/jerry_pay/dtos/response/AccountResponse.java
+++ src/main/java/com/example/jerry_pay/dtos/response/AccountResponse.java
@@ -1,4 +1,9 @@
 package com.example.jerry_pay.dtos.response;
 
+import java.math.BigDecimal;
+
 public class AccountResponse {
+    private String accountNumber;
+    private String accountName;
+    private BigDecimal balance;
 }
