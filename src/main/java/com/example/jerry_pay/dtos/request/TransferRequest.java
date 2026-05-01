242c53891a1bfc0cb324a8a26cbf8a5487e32d96
Kuse_design <0luwakusejerry1@gmail.com>
2026-05-01 20:05:11 +0100

    "task"

--- src/main/java/com/example/jerry_pay/dtos/request/TransferRequest.java
+++ src/main/java/com/example/jerry_pay/dtos/request/TransferRequest.java
@@ -1,4 +1,10 @@
 package com.example.jerry_pay.dtos.request;
 
+import java.math.BigDecimal;
+
 public class TransferRequest {
+    private String fromAccount;
+    private String toAccount;
+    private BigDecimal amount;
+    private String pin;
 }
--- src/main/java/com/example/jerry_pay/dtos/response/TransactionResponse.java
+++ src/main/java/com/example/jerry_pay/dtos/response/TransactionResponse.java
@@ -1,4 +1,10 @@
 package com.example.jerry_pay.dtos.response;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class TransactionResponse {
+    private String type;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
 }
