e16c821d73df2a6ef4987b812b1d0ac548d60886
Kuse_design <0luwakusejerry1@gmail.com>
2026-05-01 19:02:40 +0100

    "task"

--- src/main/java/com/example/jerry_pay/JerryPayApplication.java
+++ src/main/java/com/example/jerry_pay/JerryPayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JerryPayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JerryPayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JerryPayApplication.class, args);
+    }
 
 }
