5990b12f55e2858f57428f7cca8d5823178ba6bf
Kuse_design <oluwakusejerry1@gmail.com>
2026-05-04 12:08:54 +0100

    "task"

--- src/main/java/com/example/jerry_pay/dtos/response/TransactionResponse.java
+++ src/main/java/com/example/jerry_pay/dtos/response/TransactionResponse.java
@@ -1,13 +1,3 @@
 package com.example.jerry_pay.dtos.response;
 
 import lombok.Data;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-@Data
-public class TransactionResponse {
-    private String type;
-    private BigDecimal amount;
-    private LocalDateTime timestamp;
-}
