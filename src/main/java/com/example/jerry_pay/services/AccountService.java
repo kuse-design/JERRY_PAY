ccdc07f4f7c58af76971217175f95370786cab9e
Kuse_design <oluwakusejerry1@gmail.com>
2026-05-04 13:36:33 +0100

    "task"

--- src/main/java/com/example/jerry_pay/services/AccountService.java
+++ src/main/java/com/example/jerry_pay/services/AccountService.java
@@ -1,54 +1,29 @@
 package com.example.jerry_pay.services;
 
-import com.example.jerry_pay.data.models.Account;
 import com.example.jerry_pay.data.models.User;
 import com.example.jerry_pay.data.repositories.AccountRepository;
 import com.example.jerry_pay.data.repositories.UserRepository;
 import com.example.jerry_pay.dtos.request.CreateAccountRequest;
 import com.example.jerry_pay.dtos.response.AccountResponse;
-import com.example.jerry_pay.exception.AccountNotFoundException;
-import com.example.jerry_pay.exception.userNotFoundException;
 import com.example.jerry_pay.utils.AccountNumberGenerator;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 
 @Service
 public class AccountService {
-
-    private final AccountRepository accountRepository;
+    private final AccountRepository acccountRepository;
+    private final UserRepository userRepository;
     private final AccountNumberGenerator generator;
 
-    public AccountService(AccountRepository accountRepository,
-                          AccountNumberGenerator generator) {
-        this.accountRepository = accountRepository;
+    public  AccountService(AccountRepository accountRepository, UserRepository userRepository,
+                           AccountNumberGenerator generator){
+        this.acccountRepository = accountRepository;
+        this.userRepository = userRepository;
         this.generator = generator;
     }
 
-    public AccountResponse createAccount(CreateAccountRequest request) {
+    public AccountResponse CreateAccountRequest(CreateAccountRequest request){
+        User user = userRepository.findById(request.getid))
 
-        User user = UserRepository.findById(request.getUserId())
-                .orElseThrow(() -> new userNotFoundException("User not found"));
-
-        Account account = new Account();
-        account.setaccountName(request.getaccountName());
-        account.setBalance(BigDecimal.ZERO);
-        account.setPin(request.getPin());
-
-        account.setAccountNumber(generator.generate());
-
-        Account saved = accountRepository.save(account);
-
-        return AccountMapper.toResponse(saved);
-    }
-
-    public BigDecimal getBalance(String accountNumber) {
-        return findAccount(accountNumber).getBalance();
-    }
-
-
-    public Account findAccount(String accountNumber) {
-        return accountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new AccountNotFoundException("Account not found"));
     }
 }
