9aa0653dff88acd1dafbe95d6c40a507ed964c36
Kuse_design <oluwakusejerry1@gmail.com>
2026-05-05 18:30:26 +0100

    "task"

--- src/main/java/com/example/jerry_pay/services/NotificationService.java
+++ src/main/java/com/example/jerry_pay/services/NotificationService.java
@@ -1,36 +1,46 @@
 package com.example.jerry_pay.services;
 
+import com.example.jerry_pay.data.models.Account;
 import com.example.jerry_pay.data.models.Notification;
 import com.example.jerry_pay.data.models.User;
 import com.example.jerry_pay.data.models.notificationType;
 import com.example.jerry_pay.data.repositories.NotificationRepository;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public class NotificationService {
-    private final NotificationRepository notificationRepository;
+    @Service
+    public class NotificationService {
 
-    public NotificationService(NotificationRepository notificationRepository){
-        this.notificationRepository = notificationRepository;
-    }
+        private final NotificationRepository notificationRepository;
 
-    public void sendNotification(User user, BigDecimal amount, notificationType type){
-        String message;
-        if(type == notificationType.CREDIT){
-            message = ("your account has been credited with" + amount);
-        }else{
-            message = ("your account has been debited with" + amount);
+        public NotificationService(NotificationRepository notificationRepository) {
+            this.notificationRepository = notificationRepository;
         }
 
-        Notification notification = new Notification();
-        notification.setUser(user);
-        notification.setAmount(amount);
-        notification.setType(type);
-        notification.setMessage(message);
-        notification.setTimeStamp(LocalDateTime.now());
-        notificationRepository.save(notification);
+        public void sendCreditAlert(Account account, BigDecimal amount) {
+
+            Notification notification = new Notification();
+            notification.setUser(account.getUser());
+            notification.setType(notificationType.CREDIT);
+            notification.setMessage("You received " + amount);
+            notification.setTimeStamp(LocalDateTime.now());
+
+            notificationRepository.save(notification);
+        }
+
+        public void sendDebitAlert(Account account, BigDecimal amount) {
+
+            Notification notification = new Notification();
+            notification.setUser(account.getUser());
+            notification.setType(notificationType.DEBIT);
+            notification.setMessage("You spent " + amount);
+            notification.setTimeStamp(LocalDateTime.now());
+
+            notificationRepository.save(notification);
+        }
     }
 
 
-}
+
